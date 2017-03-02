3feefc279cdf9c8645468befc109a9249b9ea6b8
Bryan Jen <bryan.jen@gmail.com>
2017-03-02 14:30:57 -0700

    Add some error checking to parsers.

--- lib/puppet-gem-manager/info_parser.rb
+++ lib/puppet-gem-manager/info_parser.rb
@@ -4,8 +4,28 @@ module PuppetGemManager
   module InfoParser
 
     def self.get_info(file_path)
-      info = YAML.load_file(file_path)
-      info['info']
+      config = YAML.load_file(file_path)
+
+      # Validate that YAML was able to read something and config is not nil or false.
+      # Validate that the information structure follows the requirements.
+      if not config
+        abort 'FAILED: [InfoParser] Failed to read Information configuration file.'
+      elsif not config.has_key? 'info'
+        abort 'FAILED: [InfoParser] Info configuration is invalid. Missing top-level \'info\' key.'
+      end
+
+      info = config['info']
+      if info.nil? or info.empty?
+        abort 'FAILED: [InfoParser] Info configuration file contains no information.'
+      else
+        [:prefix, :authors, :email, :homepage, :licenses, :summary, :version].each do |required_key|
+          if not info.keys.include? required_key.to_s
+            abort "FAILED: [InfoParser] Info Configuration is missing required key of #{required_key}."
+          end
+        end
+      end
+
+      info
     end
 
   end
